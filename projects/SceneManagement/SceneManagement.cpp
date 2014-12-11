5fc97dc212d93d58dd540c98991dae087de74900
Elizabeth Lowry <elizabeth.lowry@students.aie.edu.au>
2014-12-10 18:03:42 -0800

    Continuing scene management tutorial

--- projects/SceneManagement/SceneManagement.cpp
+++ projects/SceneManagement/SceneManagement.cpp
@@ -38,6 +38,34 @@ bool SceneManagement::onCreate(int a_argc, char* a_argv[])
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
+	// load shaders and link shader program
+	m_vertShader = Utility::loadShader("shaders/lit.vert", GL_VERTEX_SHADER);
+	m_fragShader = Utility::loadShader("shaders/lit.frag", GL_FRAGMENT_SHADER);
+	const char* inputs[] = { "Position", "Normal" };
+	m_shader = Utility::createProgram(m_vertShader, 0, 0, 0, m_fragShader, 2, inputs);
+
+	m_lightAmbient = glm::vec3(0.0625, 0, 0.125);
+
+	m_lights[0].color = glm::vec3(1, 0.75, 0.875);
+	m_lights[0].position = glm::vec3(-2.4, 1.8, 4.0);
+	m_lights[0].attenuation = .5;
+	m_lights[1].color = glm::vec3(1, 0.875, 0.75);
+	m_lights[1].direction = glm::vec3(-0.48, -0.8, -0.36);
+	m_lights[1].position = glm::vec3(3.6, 6.0, 2.7);
+	m_lights[1].attenuation = 1.5;
+	m_lights[2].color = glm::vec3(0.75, 0.875, 1);
+	m_lights[2].direction = glm::vec3(0.36, -0.8, -0.48);
+	m_lights[2].position = glm::vec3(-2.7, 6.0, 3.6);
+	m_lights[2].attenuation = 1;
+	m_lights[2].angle = 45;
+	m_lights[2].blur = .1;
+	m_lightCount = 3;
+
+	m_fbx = new FBXFile();
+	m_fbx->load("models/stanford/Bunny.fbx");
+
+	createOpenGLBuffers(m_fbx);
+
 	return true;
 }
 
@@ -63,7 +91,7 @@ void SceneManagement::onUpdate(float a_deltaTime)
 	}
 
 	
-
+	/*
 	glm::vec4 plane = glm::vec4(1, 0, 0, -4.75);
 	glm::vec4 sphere = glm::vec4(-5, 0, 0, 0.5f);
 	Gizmos::addSphere(sphere.xyz(), sphere.w, 4, 4, glm::vec4(1, 1, 0, 1));
@@ -87,7 +115,7 @@ void SceneManagement::onUpdate(float a_deltaTime)
 			printf("visible\n");
 		}
 	}
-
+	/**/
 	// quit our application when escape is pressed
 	if (glfwGetKey(m_window,GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		quit();
@@ -108,12 +136,173 @@ void SceneManagement::onDraw()
 	int width = 0, height = 0;
 	glfwGetWindowSize(m_window, &width, &height);
 	Gizmos::draw2D(glm::ortho<float>(0, width, 0, height, -1.0f, 1.0f));
+
+	// get view frustrum
+	glm::vec4 planes[6];
+	getFrustrumPlanes(m_projectionMatrix, planes);
+
+	// bind shader to the GPU
+	glUseProgram(m_shader);
+
+	// fetch locations of the view and projection matrices and bind them
+	unsigned int location = glGetUniformLocation(m_shader, "view");
+	glUniformMatrix4fv(location, 1, false, glm::value_ptr(viewMatrix));
+
+	location = glGetUniformLocation(m_shader, "projection");
+	glUniformMatrix4fv(location, 1, false, glm::value_ptr(m_projectionMatrix));
+
+	// light attributes
+	location = glGetUniformLocation(m_shader, "lightAmbient");
+	glUniform3fv(location, 1, &m_lightAmbient[0]);
+	char buffer[50];
+	for (int i = 0; i < m_lightCount; ++i)
+	{
+		std::sprintf(buffer, "lights[%d].color", i);
+		location = glGetUniformLocation(m_shader, buffer);
+		glUniform3fv(location, 1, &(m_lights[i].color[0]));
+		std::sprintf(buffer, "lights[%d].direction", i);
+		location = glGetUniformLocation(m_shader, buffer);
+		glUniform3fv(location, 1, &(m_lights[i].direction[0]));
+		std::sprintf(buffer, "lights[%d].position", i);
+		location = glGetUniformLocation(m_shader, buffer);
+		glUniform3fv(location, 1, &(m_lights[i].position[0]));
+		std::sprintf(buffer, "lights[%d].power", i);
+		location = glGetUniformLocation(m_shader, buffer);
+		glUniform1f(location, m_lights[i].power);
+		std::sprintf(buffer, "lights[%d].attenuation", i);
+		location = glGetUniformLocation(m_shader, buffer);
+		glUniform1f(location, m_lights[i].attenuation);
+		std::sprintf(buffer, "lights[%d].angle", i);
+		location = glGetUniformLocation(m_shader, buffer);
+		glUniform1f(location, m_lights[i].angle);
+		std::sprintf(buffer, "lights[%d].blur", i);
+		location = glGetUniformLocation(m_shader, buffer);
+		glUniform1f(location, m_lights[i].blur);
+	}
+	location = glGetUniformLocation(m_shader, "lightCount");
+	glUniform1i(location, m_lightCount);
+
+	// send camera position
+	location = glGetUniformLocation(m_shader, "cameraPosition");
+	glUniform3fv(location, 1, glm::value_ptr(m_cameraMatrix[3]));
+
+	// bind our vertex array object and draw the mesh
+	for (unsigned int i = 0; i < m_fbx->getMeshCount(); ++i)
+	{
+		FBXMeshNode* mesh = m_fbx->getMeshByIndex(i);
+		OGL_FBXRenderData* glData = (OGL_FBXRenderData*)mesh->m_userData;
+
+		// test for visibility
+		glm::vec4 boundingSphere = glData->sphere;
+		boundingSphere.xyz = (viewMatrix * glm::vec4(boundingSphere.xyz(), 1)).xyz();
+		for (int i = 0; i < 6; ++i)
+		{
+			if (planeSphereTest(planes[i], boundingSphere) < 0)
+			{
+				printf("behind\n");
+				break;
+			}
+
+			if (i == 5)
+			{
+				printf("visible\n");
+
+				FBXMaterial* material = mesh->m_material;
+				location = glGetUniformLocation(m_shader, "hasMaterial");
+				glUniform1i(location, nullptr != material ? GL_TRUE : GL_FALSE);
+				if (nullptr != material)
+				{
+					location = glGetUniformLocation(m_shader, "materialAmbient");
+					glUniform4fv(location, 1, &(material->ambient[0]));
+					location = glGetUniformLocation(m_shader, "materialDiffuse");
+					glUniform4fv(location, 1, &(material->diffuse[0]));
+					location = glGetUniformLocation(m_shader, "materialSpecular");
+					glUniform4fv(location, 1, &(material->specular[0]));
+				}
+
+				glBindVertexArray(glData->VAO);
+				glDrawElements(GL_TRIANGLES, (unsigned int)mesh->m_indices.size(), GL_UNSIGNED_INT, 0);
+			}
+		}
+	}
 }
 
 void SceneManagement::onDestroy()
 {
 	// clean up anything we created
 	Gizmos::destroy();
+
+	cleanupOpenGLBuffers(m_fbx);
+	m_lightCount = 0;
+}
+
+void SceneManagement::createOpenGLBuffers(FBXFile* a_fbx)
+{
+	// create the GL VAO/VBO/IBO data for meshes
+	for (unsigned int i = 0; i < a_fbx->getMeshCount(); ++i)
+	{
+		FBXMeshNode* mesh = a_fbx->getMeshByIndex(i);
+
+		// storage for the opengl data in 3 unsigned int
+		OGL_FBXRenderData* glData = new OGL_FBXRenderData();
+
+		glGenVertexArrays(1, &glData->VAO);
+		glBindVertexArray(glData->VAO);
+
+		glGenBuffers(1, &glData->VBO);
+		glGenBuffers(1, &glData->IBO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, glData->VBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, glData->IBO);
+
+		glBufferData(GL_ARRAY_BUFFER, mesh->m_vertices.size() * sizeof(FBXVertex), mesh->m_vertices.data(), GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh->m_indices.size() * sizeof(unsigned int), mesh->m_indices.data(), GL_STATIC_DRAW);
+
+		// calculate bounding box
+		glm::vec3 min, max;
+		min = max = mesh->m_vertices[0].position.xyz;
+		for each (FBXVertex vertex in mesh->m_vertices)
+		{
+			if (vertex.position.x > max.x) max.x = vertex.position.x;
+			else if (vertex.position.x < min.x) min.x = vertex.position.x;
+			if (vertex.position.y > max.y) max.y = vertex.position.y;
+			else if (vertex.position.y < min.y) min.y = vertex.position.y;
+			if (vertex.position.z > max.z) max.z = vertex.position.z;
+			else if (vertex.position.z < min.z) min.z = vertex.position.z;
+		}
+
+		// calculate bounding sphere
+		glData->sphere.xyz = (min + max) / 2.0f;
+		glData->sphere.w = (max - min).length() / 2;
+
+		glEnableVertexAttribArray(0); // position
+		glEnableVertexAttribArray(1); // normal
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(FBXVertex), 0);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_TRUE, sizeof(FBXVertex), ((char*)0) + FBXVertex::NormalOffset);
+
+		glBindVertexArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+		mesh->m_userData = glData;
+	}
+}
+
+void SceneManagement::cleanupOpenGLBuffers(FBXFile* a_fbx)
+{
+	// bind our vertex array object and draw the mesh
+	for (unsigned int i = 0; i < a_fbx->getMeshCount(); ++i)
+	{
+		FBXMeshNode* mesh = a_fbx->getMeshByIndex(i);
+
+		OGL_FBXRenderData* glData = (OGL_FBXRenderData*)mesh->m_userData;
+
+		glDeleteVertexArrays(1, &glData->VAO);
+		glDeleteBuffers(1, &glData->VBO);
+		glDeleteBuffers(1, &glData->IBO);
+
+		delete glData;
+	}
 }
 
 // main that controls the creation/destruction of an application
--- projects/SceneManagement/SceneManagement.h
+++ projects/SceneManagement/SceneManagement.h
@@ -1,7 +1,34 @@
 #pragma once
 
 #include "Application.h"
+#include "FBXFile.h"
 #include <glm/glm.hpp>
+
+// struct describing a light source
+struct Light
+{
+	glm::vec3 color = glm::vec3(1, 1, 1);
+	glm::vec3 direction = glm::vec3(0, 0, 0);	// zero vector = point light
+	glm::vec3 position = glm::vec3(0, 0, 0);
+
+	// intensity = power / ((distance)^(2 * attenuation))
+	float power = 1;		// usually 1 if there's no attenuation
+	float attenuation = 0;	// 0 means no attenuation
+
+	// only used for spot lights:
+	float angle = 0;	// angle between axis and edge of spot light cone, 0 = directional light
+	float blur = 0;		// 0 = sharp cutoff, 1 = radial gradient
+};
+
+// mesh user data
+struct OGL_FBXRenderData
+{
+	unsigned int VBO; // vertex buffer object
+	unsigned int IBO; // index buffer object
+	unsigned int VAO; // vertex array object
+	glm::vec4 sphere; // bounding sphere
+};
+
 
 // derived application class that wraps up all globals neatly
 class SceneManagement : public Application
@@ -25,7 +52,22 @@ protected:
 	int planeSphereTest(const glm::vec4& a_plane, const glm::vec4& a_sphere);
 	void getFrustrumPlanes(const glm::mat4& a_transform, glm::vec4* a_planes);
 
+	void createOpenGLBuffers(FBXFile* a_fbx);
+	void cleanupOpenGLBuffers(FBXFile* a_fbx);
+
 
 	glm::mat4	m_cameraMatrix;
 	glm::mat4	m_projectionMatrix;
+
+	unsigned int m_vertShader;
+	unsigned int m_fragShader;
+	unsigned int m_shader;
+
+	glm::vec3	m_lightAmbient;
+
+	static const int MAX_LIGHTS = 10;
+	Light m_lights[MAX_LIGHTS];
+	int m_lightCount = 0;
+
+	FBXFile* m_fbx;
 };
